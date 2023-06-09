5d7a4b1b865a4ac7651e660339552dd437fcb285
9898d <shiganov.da@edu.spbstu.ru>
2023-06-09 21:01:02 +0300

    Шаблонизируй это

--- src/presenter/presenter-trip-events.js
+++ src/presenter/presenter-trip-events.js
@@ -1,10 +1,12 @@
 import NewSortView from '../view/sort-view';
 import NewFormCreateView from '../view/form-create-view';
 import NewListView from '../view/list-view';
-import NewFormEditView from '../view/form-edit-view';
+//import NewFormEditView from '../view/form-edit-view';
 import NewPointView from '../view/point-view';
 
 import { render } from '../render';
+
+import PointsModel from '../model/points-model';
 
 export default class Presenter {
 
@@ -14,12 +16,12 @@ export default class Presenter {
     this.container = container;
 
     render(new NewSortView(), this.container);
-    render(new NewFormCreateView(), this.container);
     render(this.list, this.container);
-    render(new NewFormEditView(), this.list.getElement());
+    render(new NewFormCreateView((new PointsModel).getPoints()[0]), this.list.getElement());
+    //render(new NewFormEditView(), this.list.getElement());
 
-    for (let i = 0; i < 3; i++) {
-      render(new NewPointView(), this.list.getElement());
+    for (const point of (new PointsModel).getPoints()) {
+      render(new NewPointView(point), this.list.getElement());
     }
   };
 }
