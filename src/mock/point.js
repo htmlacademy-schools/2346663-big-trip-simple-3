b6030410d419e08c36cf9217993b1e7de1c9594d
9898d <shiganov.da@edu.spbstu.ru>
2023-06-13 02:38:37 +0300

    Большие перемены (часть 2)

--- src/mock/point.js
+++ src/mock/point.js
@@ -1,9 +1,9 @@
 export const TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
 
 export const genereatePoint = (destination, offers, id) => ({
-  basePrice: 12345,
-  dateFrom: '2019-07-10T22:55:56.845Z',
-  dateTo: '2019-07-11T11:22:13.375Z',
+  basePrice: 10000 - id,
+  dateFrom: `2019-07-${id + 10}T22:55:56.845Z`,
+  dateTo: `2019-07-${id + 10}T11:22:13.375Z`,
   destination: destination,
   id: id,
   offers: offers.offers,
